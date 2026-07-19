68b0d61b62267dbc746a418617ef000593366746
KARAN SHANKAR TRIPATHI <129868945+Karan-Trips@users.noreply.github.com>
2026-07-19 19:18:41 +0530

    Update the main File

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: AuraExpenseApp()));
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const ProviderScope(child: AuraExpenseApp()));
+    },
+    (error, stack) {
+      debugPrint('Uncaught asynchronous error: $error');
+      debugPrint(stack.toString());
+    },
+  );
 }
